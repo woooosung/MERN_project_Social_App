503ff239d377955397700bb223b2374b407a5987
Woo Sung Kang <geniusraban@gmail.com>
2022-07-10 17:25:42 +0900

    dispatch

--- server/index.js
+++ server/index.js
@@ -5,10 +5,14 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
-const app = express();
-
 const CONNECTION_URL = 'mongodb+srv://WooSung:fPYL0Z1vtBbEvl88@cluster0.evhysaf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended : true }));
+app.use(bodyParser.urlencoded({limit: "30mb", extended : true }));
+app.use(cors());
 
 app.use('/posts', postRoutes);
 
@@ -17,7 +21,4 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology:tr
     .catch((error) => console.log(error.message))
 
 
-app.use(bodyParser.json({limit: "30mb", extended : true }));
-app.use(bodyParser.urlencoded({limit: "30mb", extended : true }));
-app.use(cors());
 
