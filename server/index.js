2292ae8c94003c208937ff50c842016aaa432fbd
Woo Sung Kang <geniusraban@gmail.com>
2022-07-12 01:22:03 +0900

    improvements

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
-const CONNECTION_URL = 'mongodb+srv://WooSung:fPYL0Z1vtBbEvl88@cluster0.evhysaf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+dotenv.config({path:'config.env'});
 
 app.use(bodyParser.json({limit: "30mb", extended : true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended : true }));
@@ -16,7 +17,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology:true})
     .then(()=> app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`)))
     .catch((error) => console.log(error.message))
 
